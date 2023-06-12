2d1dcaccb2d141c11004f9ede18f56d49b6bc404
Durgeshkumar1113 <75772172+Durgeshkumar1113@users.noreply.github.com>
2023-06-12 10:25:27 +0530

    Update Assignment4.js

---
+++ Assignment4.js
@@ -0,0 +1,12 @@
+let fruits="banana"
+switch(fruits){
+    case "apple" :
+        console.log("apple")
+        break;
+    case "banana" :
+        console.log("banana")
+        break;
+    case "orange" :
+        console.log("orange")
+        break;
+}
